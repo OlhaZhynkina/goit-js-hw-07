887aabac0912d8e4470c313be4ad85727351df3a
Olha <olyasavvz@gmail.com>
2024-06-29 14:15:04 +0300

    comments

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,7 @@
 const categories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categories.length}`);
 
-
-categories.forEach((category) => {
-    console.log(`Category: ${category.firstElementChild.textContent}`);
-    console.log(`Elements: ${category.lastElementChild.childElementCount}`);
+categories.forEach(category => {
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements: ${category.lastElementChild.childElementCount}`);
 });
