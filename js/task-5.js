942c90c73600ece31ca7407f6b69b622e2f7eea6
Olha <olyasavvz@gmail.com>
2024-07-01 23:48:36 +0300

    task5-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanElement = document.querySelector('.color');
+const buttonElement = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+buttonElement.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  spanElement.style.color = newColor;
+  body.style.backgroundColor = newColor;
+}
